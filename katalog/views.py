271dcfcee19fc8c985f2ea0eca8bbf310c195ef0
Felix Tjahyadi <felix.tjahyadi@ui.ac.id>
2022-09-12 10:47:02 +0700

    Tugas 2 PBP

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_katalog_item = CatalogItem.objects.all()
+context = {
+    'list_katalog': data_katalog_item,
+    'nama': 'Felix Tjahyadi',
+    'student_id': '2106638614'
+}
