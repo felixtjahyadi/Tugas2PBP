6166b3853c689864ac586f60d19e47b590ab7a45
Felix Tjahyadi <felix.tjahyadi@ui.ac.id>
2022-09-13 10:26:44 +0700

    Tugas 2 PBP

--- project_django/settings.py
+++ project_django/settings.py
@@ -140,3 +140,7 @@ for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
