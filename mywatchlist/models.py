dec9c2befecc5159ee217fde089bb7279c282889
Felix Tjahyadi <felix.tjahyadi@ui.ac.id>
2022-11-23 21:47:35 +0700

    Changes for Tugas 9

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class WatchListItem(models.Model):
-    watched = models.BooleanField()
+    watched = models.CharField(max_length=500)
     title = models.CharField(max_length=500)
     rating = models.IntegerField()
-    release_date = models.DateField()
+    release_date = models.CharField(max_length=500)
     review = models.TextField()
