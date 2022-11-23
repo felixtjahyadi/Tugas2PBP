c6ce80a0b3530c2fb8f78c6e1ac72224d769ffc3
Felix Tjahyadi <felix.tjahyadi@ui.ac.id>
2022-11-23 21:23:09 +0700

    Minor changes for Tugas 9

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -5,5 +5,5 @@ class WatchListItem(models.Model):
     watched = models.BooleanField()
     title = models.CharField(max_length=500)
     rating = models.IntegerField()
-    release_date = models.CharField(max_length=500)
+    release_date = models.DateField()
     review = models.TextField()
